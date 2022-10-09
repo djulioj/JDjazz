c9ae4c3182ab4543aa86d41f3d519bc384e6d677
Juan David Julio San Juan <61607858+djulioj@users.noreply.github.com>
2022-10-09 18:47:39 -0500

    Play button update

--- js/JDjazz.js
+++ js/JDjazz.js
@@ -1,21 +1,41 @@
-function loadSongs(){
+function activateMusicSpace(){
+  const Songs=document.getElementById("songs");
+  const hr=document.getElementById("line");
+  const playButton=document.getElementById("play");
+
+  Songs.classList.remove("hidden");
+  hr.classList.remove("hidden");
+  playButton.classList.remove("hidden");
+
+}
+function loadSongs(numSongs){
     const space=document.getElementById("songs");
 
     if(canciones.length>0){
-        const body='<div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" id="list">  </div></div>';
+      activateMusicSpace();
+        const body='<div class="mx-auto flex justify-center max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" id="list">  </div></div>';
         space.innerHTML+=body;
         const listHTML=document.getElementById("list");
-        for(i=(pages-1)*8;i<canciones.length && i<pages*8;i++){
-            cancion=canciones[i];
-            nameSong=cancion.nombre;
-            const song=`<div class="group cursor-pointer w-fit">
-            <div class="aspect-w-1 aspect-h-1 w-48 overflow-hidden rounded-full xl:aspect-w-7 xl:aspect-h-8">
-              <img src="./img/1366_2000.jpg " class="bg-white h-full w-full object-cover object-center group-hover:opacity-75">
-            </div>
-            <h3 class="flex justify-center text-white mt-4 text-sm">nameSong</h3>
-          </div>`;
-          listHTML.innerHTML+=song;
+        if(canciones.length===1){
+          it=0;
+        }else{
+          it=canciones.length-numSongs;
         }
+        for(i=it;i<canciones.length && i<pages*8;i++){
+          cancion=canciones[i];
+          console.log(cancion)
+          console.log(it);
+          console.log(numSongs);
+          nameSong=cancion.nombre;
+          const song=`
+            <div class="group cursor-pointer w-fit lg:ml-10 lg:mr-10" id="${nameSong}" onclick="${()=>(alert('suenala'))}">
+              <div class="aspect-w-1 aspect-h-1 w-48 overflow-hidden rounded-full xl:aspect-w-7 xl:aspect-h-8">
+                <img src="./img/1366_2000.jpg " class="bg-white h-full w-full object-cover object-center group-hover:opacity-75">             
+              </div>
+              <h3 class="flex justify-center text-white mt-4 text-sm">${nameSong}</h3>
+            </div>`;
+            listHTML.innerHTML+=song;
+          }
     }
 };
 
@@ -25,7 +45,6 @@ function buttons(){
     addButton.addEventListener("change",(f)=>{
         const files=f.target.files;
         const songs=[];
-
         for(i=0;i<files.length;i++){
             const file=files[i];
             if(file.type.includes("audio")){
@@ -40,8 +59,15 @@ function buttons(){
         }
 
         canciones=canciones.concat(songs);
-        loadSongs();
+        console.log(canciones);
+        loadSongs(songs.length);
     });
+}
+
+function playSong(){
+  
+    console.log("suenala reyy");
+  
 }
 
 var canciones=[];
