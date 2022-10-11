c6da6137721819262f93c6b56801be8feccee032
slanchero <slanchero@uninorte.edu.co>
2022-10-10 23:57:33 -0500

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+function playMusic(index){
+    const playImg=document.getElementById("playimg");
+    playImg.src="../img/pause.png";
+    url=canciones[index].url;
+    audio.src=url;
+    audio.play();
+}
+
 function buttons(){
     const addBtn=document.getElementById("addButton");
     const playBtn=document.getElementById("playBtn");
@@ -25,7 +33,6 @@ function buttons(){
         }
         document.getElementById("inputTag").value="";
         canciones=canciones.concat(songs);
-        console.log(canciones);
         load();
     });
 
@@ -41,7 +48,7 @@ function buttons(){
             audio.play();
             playImg.src="../img/pause.png";
             playing=true;
-            }else{
+        }else{
             audio.pause();
             playImg.src="../img/boton-de-play.png";
             playing=false;
@@ -54,7 +61,7 @@ function buttons(){
         }else{
             playingSong--;
         }
-        console.log(playingSong);
+        playMusic(playingSong);
     });
 
     nextBtn.addEventListener("click",(e)=>{
@@ -63,23 +70,27 @@ function buttons(){
         }else{
             playingSong++;
         }
-        console.log(playingSong);
+        playMusic(playingSong);
     });
 
     spaceList.addEventListener("click",(e)=>{
         if(e.target.classList.contains("song")){
-            console.log(e.target.getAttribute("cancion"));
+            if(e.target.classList.contains("song")){
+                playingSong=e.target.getAttribute("cancion");
+                playing=true;
+                playMusic(e.target.getAttribute("cancion"));
+            }
         }
     });
 
     prevPage.addEventListener("click",(e)=>{
         page--;  
-        console.log(page);  
-    });
+        load();
+    });  
 
     nextPage.addEventListener("click",(e)=>{
         page++; 
-        console.log(page);    
+        load();  
     });
 }
 
@@ -91,14 +102,29 @@ function btnNoList(){
 
 function btnWithList(){
     const reproductor=document.getElementById("reproductor");
+    const nextPage=document.getElementById("nextpage");
+    const prevPage=document.getElementById("prevpage");
+    const textPage=document.getElementById("textPage");
     
     reproductor.classList.remove("hidden");
+    
+    textPage.innerHTML=`<h2 class="text-white justify-center text-center flex items-center mb-10">${page} of ${totalPages}</h2>`;
+
+    if(page==1){
+        prevPage.classList.add("hidden");
+    }else{
+        prevPage.classList.remove("hidden");
+    }
+    if(page==totalPages){
+        nextPage.classList.add("hidden");
+    }else{
+        nextPage.classList.remove("hidden");
+    }
 }
 
 function load(){
     const space=document.getElementById("songs");
     if(canciones.length>0){
-        btnWithList();
         const body='<div class="mx-auto flex justify-center max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" id="list">  </div></div>';
         space.innerHTML=body;
         const listHTML=document.getElementById("list");
@@ -107,14 +133,16 @@ function load(){
           nameSong=cancion.nombre;
           urlSong=cancion.url;
           const song=`
-            <div class="group w-fit lg:ml-10 lg:mr-10 song songCard" cancion=${nameSong}>
-              <div cancion=${nameSong} class="aspect-w-1 cursor-pointer aspect-h-1 w-48 overflow-hidden rounded-full xl:aspect-w-7 xl:aspect-h-8 song" id="${urlSong}">
-                <img cancion=${nameSong} src="./img/1366_2000.jpg " class="flex justify-center  bg-white h-full w-full object-cover object-center group-hover:opacity-75 song">             
+            <div class="group w-fit lg:ml-10 lg:mr-10 song songCard" cancion=${i}>
+              <div cancion=${i} class="aspect-w-1 cursor-pointer aspect-h-1 w-48 overflow-hidden rounded-full xl:aspect-w-7 xl:aspect-h-8 song" id="${urlSong}">
+                <img cancion=${i} src="./img/1366_2000.jpg " class="flex justify-center  bg-white h-full w-full object-cover object-center group-hover:opacity-75 song">             
               </div>
-              <h3 class="flex justify-center text-white mt-4 text-sm song" cancion=${nameSong}>${nameSong}</h3>
+              <h3 class="flex justify-center text-white mt-4 text-sm song" cancion=${i}>${nameSong}</h3>
             </div>`;
           listHTML.innerHTML+=song;        
-          }
+        }
+        totalPages=~~(canciones.length/8)+1;
+        btnWithList();
     }else{
         btnNoList();
     }
