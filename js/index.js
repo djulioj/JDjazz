ca643ee2517d84cfdca5ba876870f483520eeef7
slanchero <slanchero@uninorte.edu.co>
2022-10-10 23:04:28 -0500

    update index.js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ function buttons(){
     const nextBtn=document.getElementById("nextBtn");
     const prevBtn=document.getElementById("prevBtn");
     const spaceList=document.getElementById("songs");
+    const nextPage=document.getElementById("nextpage");
+    const prevPage=document.getElementById("prevpage");
 
     addBtn.addEventListener("change",(e)=>{
         const files=e.target.files;
@@ -24,6 +26,7 @@ function buttons(){
         document.getElementById("inputTag").value="";
         canciones=canciones.concat(songs);
         console.log(canciones);
+        load();
     });
 
     playBtn.addEventListener("click",(e)=>{
@@ -46,8 +49,75 @@ function buttons(){
     });
 
     prevBtn.addEventListener("click",(e)=>{
-        
+        if(playingSong===null || playingSong<=0){
+            playingSong=canciones.length-1;
+        }else{
+            playingSong--;
+        }
+        console.log(playingSong);
     });
+
+    nextBtn.addEventListener("click",(e)=>{
+        if(playingSong>=canciones.length-1){
+            playingSong=0;
+        }else{
+            playingSong++;
+        }
+        console.log(playingSong);
+    });
+
+    spaceList.addEventListener("click",(e)=>{
+        if(e.target.classList.contains("song")){
+            console.log(e.target.getAttribute("cancion"));
+        }
+    });
+
+    prevPage.addEventListener("click",(e)=>{
+        page--;  
+        console.log(page);  
+    });
+
+    nextPage.addEventListener("click",(e)=>{
+        page++; 
+        console.log(page);    
+    });
+}
+
+function btnNoList(){
+    const reproductor=document.getElementById("reproductor");
+    
+    reproductor.classList.add("hidden");
+}
+
+function btnWithList(){
+    const reproductor=document.getElementById("reproductor");
+    
+    reproductor.classList.remove("hidden");
+}
+
+function load(){
+    const space=document.getElementById("songs");
+    if(canciones.length>0){
+        btnWithList();
+        const body='<div class="mx-auto flex justify-center max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" id="list">  </div></div>';
+        space.innerHTML=body;
+        const listHTML=document.getElementById("list");
+        for(i=(page-1)*8;i<canciones.length && i<page*8;i++){
+          cancion=canciones[i];
+          nameSong=cancion.nombre;
+          urlSong=cancion.url;
+          const song=`
+            <div class="group w-fit lg:ml-10 lg:mr-10 song songCard" cancion=${nameSong}>
+              <div cancion=${nameSong} class="aspect-w-1 cursor-pointer aspect-h-1 w-48 overflow-hidden rounded-full xl:aspect-w-7 xl:aspect-h-8 song" id="${urlSong}">
+                <img cancion=${nameSong} src="./img/1366_2000.jpg " class="flex justify-center  bg-white h-full w-full object-cover object-center group-hover:opacity-75 song">             
+              </div>
+              <h3 class="flex justify-center text-white mt-4 text-sm song" cancion=${nameSong}>${nameSong}</h3>
+            </div>`;
+          listHTML.innerHTML+=song;        
+          }
+    }else{
+        btnNoList();
+    }
 }
 
 var playing=false;
@@ -57,3 +127,4 @@ var canciones=[];
 var page=1;
 var totalPages=1;
 buttons();
+load();
